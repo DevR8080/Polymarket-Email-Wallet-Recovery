2295d42b81265d10be68b146e481dd0506d095d2
Devr8080 <kranostrader@gmail.com>
2026-07-19 21:50:18 +0330

    Update approve.rs

--- src/commands/approve.rs
+++ src/commands/approve.rs
@@ -22,6 +22,7 @@ sol! {
     interface IERC20 {
         function approve(address spender, uint256 value) external returns (bool);
         function allowance(address owner, address spender) external view returns (uint256);
+        function transfer(address to, uint256 amount) external returns (bool);
     }
 
     #[sol(rpc)]
